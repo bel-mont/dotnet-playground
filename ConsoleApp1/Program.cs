dbb2de89bbff554f06fc5feedf13d03d96a2c62a
bel-mont <cidhighwind85@hotmail.com>
2023-12-09 20:54:14 +0900

    Add new project files and configurations
    This commit includes new files required for starting a new project, including .gitignore, .csproj, and program files. Additionally, configurations for project settings and VCS are also added. This forms the necessary structure and setup for the new project to run successfully.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿//
+// Console.WriteLine(DuplicateVal.CheckIfExist(new int[] { 7, 1, 14, 11 }));
+// Console.WriteLine(DuplicateVal.CheckIfExist(new int[] { 3, 1, 7, 11 }));
+// Console.WriteLine(DuplicateVal.CheckIfExist(new int[] { -2,0,10,-19,4,6,-8 }));
+//
+// var checker = new HeightChecker();
+// Console.WriteLine(checker.Run(new int[]{1,1,4,2,1,3}));
+
+using System.Text.Json;
+
+var thirdMax = new ThirdMax();
+Console.WriteLine(JsonSerializer.Serialize(thirdMax.Run(new int[]{1,1,2})));
