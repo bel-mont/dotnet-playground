9a667c60fcceb34be2dad3ee3d32e1d8961cd999
bel-mont <cidhighwind85@hotmail.com>
2023-12-10 17:05:27 +0900

    Add Squares class in ConsoleApp1
    This new class contains a function, `SortedSquares`, that takes an integer array and returns a new array with sorted squares. Pointers from both ends of the array are used to iterate and compare values, ensuring the largest squares are placed first in the resulting array.

---
+++ ConsoleApp1/Squares.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1;
+
+public class Squares
+{
+  public int[] SortedSquares(int[] nums) {
+    var sortedSquared = new int[nums.Length];
+    // Loop until the pointers have the same index.
+    // Start indexes at beginning and end of array
+    var startIndex = 0;
+    var lastIndex = nums.Length - 1;
+    var currentSortedSquaredIndex = lastIndex;
+    while (true) {
+      if (Math.Abs(nums[startIndex]) > Math.Abs(nums[lastIndex])) {
+        sortedSquared[currentSortedSquaredIndex] = nums[startIndex] * nums[startIndex];
+        startIndex++;
+        currentSortedSquaredIndex--;
+      } else {
+        sortedSquared[currentSortedSquaredIndex] = nums[lastIndex] * nums[lastIndex];
+        lastIndex--;
+        currentSortedSquaredIndex--;
+      }
+      if (startIndex >= lastIndex) {
+        sortedSquared[Math.Max(currentSortedSquaredIndex, 0)] = nums[startIndex] * nums[startIndex];
+        break;
+      }
+    }
+    return sortedSquared;
+  }
+}
