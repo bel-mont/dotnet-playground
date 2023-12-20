b6e13bfce7665427db4efbaba9018d1cb0ef6a62
bel-mont <cidhighwind85@hotmail.com>
2023-12-20 21:58:19 +0900

    Add PrefixSum and SlidingWindow classes

---
+++ ConsoleApp1/PrefixSum.cs
@@ -0,0 +1,27 @@
+﻿public class PrefixSum
+{
+  public int MinStartValue(int[] nums) {
+    // the validity condition is sum > 0
+    // the first case of our sum never reaching 0 or less is our result
+    var startValue = 1;
+    bool sumBelowOne = true;
+    while (sumBelowOne)
+    {
+      var sum = startValue;
+      sumBelowOne = false;
+      for (var i = 0; i < nums.Length; i++)
+      {
+        sum += nums[i];
+        if (sum < 1)
+        {
+          // we break out and go into the next startValue
+          startValue++;
+          sumBelowOne = true;
+          break;
+        }
+      }
+    }
+      
+    return startValue;
+  }
+}
---
+++ ConsoleApp1/SlidingWindow.cs
@@ -0,0 +1,58 @@
+﻿namespace ConsoleApp1;
+
+public class SlidingWindow
+{
+  public double FindMaxAverage(int[] nums, int k) {
+    if (nums.Length == 1)
+    {
+      return nums[0];
+    }
+    double currentSum = 0;
+    double maxAverage = 0;
+    // get average of first k items
+    for (var i = 0; i < k; i++)
+    {
+      currentSum += nums[i];
+    }
+    maxAverage = currentSum / k;
+      
+    // Then loop the rest of the array, sliding with a k sized window
+    for (var i = k; i < nums.Length; i++)
+    {
+      // add item on the right, remove item on the left
+      // nums[i - k] to remove the item furthest to the left of the current window
+      currentSum += nums[i] - nums[i - k];
+      // then get average
+      double average = currentSum / k;
+      // then compare to previous maxAverage
+      maxAverage = Math.Max(maxAverage, average);
+    }
+    // Check edge cases later
+    return maxAverage;
+  }
+  
+  public int LongestOnes(int[] nums, int k) {
+    // create a window that increases in size until it finds more than k amount of 0s
+    var maxConsecutiveOnes = 0;
+    var zerosInCurrentWindow = 0;
+    var j = 0;
+    for (var i = 0; i < nums.Length; i++)
+    {
+      if (nums[i] == 0)
+      {
+        zerosInCurrentWindow++;
+      }
+      while (zerosInCurrentWindow > k)
+      {
+        if (nums[j] == 0)
+        {
+          zerosInCurrentWindow--;
+        }
+        j++;
+      }
+      maxConsecutiveOnes = Math.Max(maxConsecutiveOnes, i - j + 1);
+    }
+      
+    return maxConsecutiveOnes;
+  }
+}
