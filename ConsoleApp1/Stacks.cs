2b7bd4cad441bcf215ed0718850a1cf085e55304
bel-mont <cidhighwind85@hotmail.com>
2024-01-31 22:21:28 +0900

    Add MovingAverage class to Stacks.cs
    A new class, MovingAverage, has been added to the Stacks.cs file. This class is designed to handle a queue of a defined size, and provide a rolling average (moving average) of the items within. This may be useful for maintaining and processing a dynamic mean in streaming data processing scenarios.

--- ConsoleApp1/Stacks.cs
+++ ConsoleApp1/Stacks.cs
@@ -19,3 +19,24 @@
  * RecentCounter obj = new RecentCounter();
  * int param_1 = obj.Ping(t);
  */
+ 
+public class MovingAverage {
+  public Queue<int> queue = new Queue<int>();
+  public int queueSize;
+  public int sum = 0;
+
+  public MovingAverage(int size) {
+    queueSize = size;
+  }
+
+  public double Next(int val) {
+    queue.Enqueue(val);
+    while (queue.Count > queueSize)
+    {
+      var removedVal = queue.Dequeue();
+      sum -= removedVal;
+    }
+    sum += val;    
+    return (double)sum / (double)queue.Count;
+  }
+}
