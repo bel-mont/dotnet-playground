386e149dad8d7ab6f111477e57ac041807c381bd
bel-mont <cidhighwind85@hotmail.com>
2024-01-31 21:53:18 +0900

    Add RecentCounter class to manage a queue
    A new file, Stacks.cs, has been added, which includes the recently created RecentCounter class. This class employs a queue for processing integer values and spam protection by removing items from the front of the queue, maintaining the recentness of data.

---
+++ ConsoleApp1/Stacks.cs
@@ -0,0 +1,21 @@
+﻿public class RecentCounter {
+  public Queue<int> queue = new Queue<int>();
+    
+  public int Ping(int t) {
+    // remove items from the start of the queue when its value is less
+    // than the last item value - 3000
+    while (queue.Count > 0 && queue.Peek() < t - 3000)
+    {
+      queue.Dequeue();
+    }
+
+    queue.Enqueue(t);
+    return queue.Count;
+  }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.Ping(t);
+ */
