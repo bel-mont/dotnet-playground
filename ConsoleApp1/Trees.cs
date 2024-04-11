84018e5b561f7e937988117ae7a637b9c12cbe35
bel-mont <cidhighwind85@hotmail.com>
2024-04-11 22:04:48 +0900

    more binary tree problems

--- ConsoleApp1/Trees.cs
+++ ConsoleApp1/Trees.cs
@@ -77,3 +77,72 @@ public class Leetcode102 {
     return answer;
   }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Leetcode1161 {
+  public int MaxLevelSum(TreeNode root) {
+    // BFS
+    var answer = 0;
+    if (root == null) return answer;
+    var queue = new Queue<TreeNode>();
+    queue.Enqueue(root);
+    var currMax = Int32.MinValue;
+    var level = 1;
+    while (queue.Count > 0)
+    {
+      var levelSize = queue.Count;
+      var levelSum = 0;
+      for (var i = 0; i < levelSize; i++)
+      {
+        var node = queue.Dequeue();
+        levelSum += node.Val;
+          
+        if (node.Left != null) queue.Enqueue(node.Left);
+        if (node.Right != null) queue.Enqueue(node.Right);
+      }
+      if (levelSum > currMax)
+      {
+        answer = level;
+        currMax = levelSum;
+      }
+      level++;
+    }
+
+    return answer;
+  }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Leetcode1325 {
+  public TreeNode RemoveLeafNodes(TreeNode root, int target) {
+    if (root == null) return null;
+    if (root.Left != null) root.Left = RemoveLeafNodes(root.Left, target);
+    if (root.Right != null) root.Right = RemoveLeafNodes(root.Right, target);
+    if (root.Val == target && root.Left == null && root.Right == null) return null;
+    return root;
+  }
+}
