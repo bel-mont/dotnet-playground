fb03ef39614c63c6b69faa17a37aed121881eacb
bel-mont <cidhighwind85@hotmail.com>
2024-04-25 21:28:28 +0900

    leetcode 2208

--- ConsoleApp1/Heap.cs
+++ ConsoleApp1/Heap.cs
@@ -7,6 +7,7 @@ class MaxHeapComparer : IComparer<int>
     }
 }
 
+
 public class Leetcode1046 {
     public int LastStoneWeight(int[] stones) {
         var heap = new PriorityQueue<int, int>(new MaxHeapComparer());
@@ -22,5 +23,38 @@ public class Leetcode1046 {
             if (first != second) heap.Enqueue(first - second, first - second);
         }
         return heap.Count == 0 ? 0 : heap.Dequeue();
+    }
+}
+
+public class Leetcode2208 {
+    public int HalveArray(int[] nums) {
+        double target = 0;
+        var heap = new PriorityQueue<double, double>(new MaxHeapComparerDouble());
+        foreach (var n in nums)
+        {
+            target += n;
+            heap.Enqueue(n, n);
+        }
+
+        // need to have the sum
+        target = target / 2;
+        var ans = 0;
+        while (target > 0)
+        {
+            ans++;
+            var x = heap.Dequeue();
+            var half = x / 2;
+            target -= half;
+            heap.Enqueue(half, half);
+        }
+        return ans;
+    }
+
+    class MaxHeapComparerDouble : IComparer<double>
+    {
+        public int Compare(double x, double y)
+        {
+            return y.CompareTo(x);
+        }
     }
 }
