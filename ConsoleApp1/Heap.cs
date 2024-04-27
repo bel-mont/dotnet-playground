83d4c9326c06f4808ddcadc6c09eed81598104f6
bel-mont <bel-mont@users.noreply.github.com>
2024-04-27 13:29:12 +0900

    leetcode 703

--- ConsoleApp1/Heap.cs
+++ ConsoleApp1/Heap.cs
@@ -246,3 +246,29 @@ public class Leetcode973 {
         return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
     }
 }
+
+public class KthLargest {
+    private PriorityQueue<int, int> heap = new PriorityQueue<int, int>();
+    private int _k;
+  
+    public KthLargest(int k, int[] nums) {
+        _k = k;
+        foreach (var n in nums)
+        {
+            heap.Enqueue(n, n);
+            if (heap.Count > k) heap.Dequeue();
+        }
+    }
+  
+    public int Add(int val) {
+        heap.Enqueue(val, val);
+        if (heap.Count > _k) heap.Dequeue();
+        return heap.Peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.Add(val);
+ */
