3d8d90ebbaf55fb7cedc2f448a7e24631aeaa8f5
bel-mont <cidhighwind85@hotmail.com>
2024-07-17 21:18:58 +0900

    standard binary search

--- ConsoleApp1/BinarySearch.cs
+++ ConsoleApp1/BinarySearch.cs
@@ -95,3 +95,19 @@ public class Leetcode2300 {
         return left;
     }
 }
+
+public class Leetcode35 {
+    public int SearchInsert(int[] nums, int target) {
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left <= right)
+        {
+            var mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid; // found
+            else if (target > nums[mid]) left = mid + 1; // target is greater
+            else right = mid - 1; // target is smaller
+        }
+
+        return left;
+    }
+}
